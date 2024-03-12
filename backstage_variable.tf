3ad8e2e5f6b7a56cbe4776183e2a87e64070ad0a
pshdevops <79852948+pshdevops@users.noreply.github.com>
2024-03-12 11:37:04 +0000

    🔩 Create 123456 AWS SQS

--- backstage_variable.tf
+++ backstage_variable.tf
@@ -1,7 +1,7 @@
 variable "account_id" {
   type        = string
   description = ""
-  default     = "12345678"
+  default     = "123456"
 }
 
 variable "user_access" {
@@ -14,5 +14,5 @@ variable "user_access" {
 variable "s3_folders" {
   type        = list(string)
   description = "s3 folders"
-  default     = [ abcdge,acbdgh ]
+  default     = ${ JSON.stringify(values.s3_folders) }
 }
