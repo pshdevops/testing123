7b7a5eba8cb93dd53fe5ffe92faf0b73c4812c6c
pshdevops <79852948+pshdevops@users.noreply.github.com>
2024-03-15 07:07:07 +0000

    🔩 Create 1234567890 AWS

--- backstage_variable.tf
+++ backstage_variable.tf
@@ -1,7 +1,7 @@
 variable "account_id" {
   type        = string
   description = ""
-  default     = "12345678"
+  default     = "1234567890"
 }
 
 variable "user_access" {
@@ -10,12 +10,11 @@ variable "user_access" {
   default     = "readOnly"
 }
 
-
 variable "s3_folders" {
   type        = list(string)
   description = "s3 folders"
   default     =  [
-    "abcdge",
-    "acbdgh",
+    "bucket1",
+    "acbdghe",
   ]
 }
