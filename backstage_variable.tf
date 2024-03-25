b382701bfc1d718b8c18b02e313874034766927d
pshdevops <79852948+pshdevops@users.noreply.github.com>
2024-03-25 13:26:17 +0000

    🔩 Create harsha-dpa123456778 AWS

--- backstage_variable.tf
+++ backstage_variable.tf
@@ -1,9 +1,3 @@
-variable "account_id" {
-  type        = string
-  description = ""
-  default     = "12345678901"
-}
-
 variable "user_access" {
   type        = string
   description = "user access"
@@ -14,12 +8,12 @@ variable "s3_folders" {
   type        = list(string)
   description = "s3 folders"
   default     =  [
-    "bucket3",
-    "bucket4",
+    "bucket1",
+    "bucket2",
   ]
 }
 
 variable "dpa_bucket_name" {
   type = string
-  default = ""
+  default     = "harsha-dpa123456778"
 }
